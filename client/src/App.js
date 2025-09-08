91fd12b01b82af89953247a919d5e46c8af3dfa5
navgr2001 <naveenshalom2001@gmail.com>
2025-09-08 21:23:52 +0530

    flexbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import createTheme from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import { themeSettings } from "theme";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -14,7 +17,10 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route element={<Layout />}></Route>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
