4797950ee4724d9fc4c0344249325610b95f3f08
navgr2001 <naveenshalom2001@gmail.com>
2025-09-08 23:41:51 +0530

    error fix

--- client/src/components/DataGridCustomToolbar.jsx
+++ client/src/components/DataGridCustomToolbar.jsx
@@ -1,47 +1,23 @@
+// src/components/DataGridCustomColumnMenu.jsx
 import React from "react";
-import { Search } from "@mui/icons-material";
-import { IconButton, TextField, InputAdornment } from "@mui/material";
 import {
-  GridToolbarDensitySelector,
-  GridToolbarContainer,
-  GridToolbarExport,
-  GridToolbarColumnsButton,
+  GridColumnMenuContainer,
+  GridColumnMenuFilterItem,
+  GridColumnMenuHideItem,
+  GridColumnMenuColumnsItem,
+  GridColumnMenuSortItem,
 } from "@mui/x-data-grid";
-import FlexBetween from "./FlexBetween";
 
-const DataGridCustomToolbar = ({ searchInput, setSearchInput, setSearch }) => {
+const CustomColumnMenu = (props) => {
+  const { hideMenu, currentColumn } = props;
   return (
-    <GridToolbarContainer>
-      <FlexBetween width="100%">
-        <FlexBetween>
-          <GridToolbarColumnsButton />
-          <GridToolbarDensitySelector />
-          <GridToolbarExport />
-        </FlexBetween>
-        <TextField
-          label="Search..."
-          sx={{ mb: "0.5rem", width: "15rem" }}
-          onChange={(e) => setSearchInput(e.target.value)}
-          value={searchInput}
-          variant="standard"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  onClick={() => {
-                    setSearch(searchInput);
-                    setSearchInput("");
-                  }}
-                >
-                  <Search />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-      </FlexBetween>
-    </GridToolbarContainer>
+    <GridColumnMenuContainer {...props}>
+      <GridColumnMenuSortItem onClick={hideMenu} column={currentColumn} />
+      <GridColumnMenuFilterItem onClick={hideMenu} column={currentColumn} />
+      <GridColumnMenuHideItem onClick={hideMenu} column={currentColumn} />
+      <GridColumnMenuColumnsItem onClick={hideMenu} />
+    </GridColumnMenuContainer>
   );
 };
 
-export default DataGridCustomToolbar;
+export default CustomColumnMenu;
