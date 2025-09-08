837b1ef9be8848e4a1bd9e52f1a779844d888ae2
navgr2001 <naveenshalom2001@gmail.com>
2025-09-08 19:39:18 +0530

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import createTheme from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+
 function App() {
-  return <div className="app"></div>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}></Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
